b5e6a16d31627607ce3e38937a389c2a152ce70c
MK <mrxkollo@users.noreply.github.com>
2017-05-10 14:43:46 +0300

    Spring Boot + class for Square

---
+++ src/main/java/t09tervikveeb/MarkoApplication.java
@@ -0,0 +1,12 @@
+package t09tervikveeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkoApplication.class, args);
+	}
+}
