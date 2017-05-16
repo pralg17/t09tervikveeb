44b09988dcadf1eef3fc75884c16ada746eff1b8
martkasa <martkasa@greeny.cs.tlu.ee>
2017-05-16 14:40:45 +0300

    farn,celsius, kelvin

---
+++ src/main/java/martin/Faren.java
@@ -0,0 +1,10 @@
+package martin;
+
+public class Faren{
+	public double CelsiusFaren(double x){
+		return ((x-32)*5)/9 ;
+	}
+	public double KelvinFaren(double x){
+		return ((x + 459.67)*5)/9 ;
+	}
+}
