ac6cf85f877d640f8d34f4e2840cb73dd40880f7
atsklem <atsklem@greeny.cs.tlu.ee>
2017-05-28 17:17:55 +0300

    Adding hw

---
+++ src/main/java/ats/pyramiid.java
@@ -0,0 +1,17 @@
+package ats;
+
+public class pyramiid extends pystkeha{
+	double a, k, kK;
+	public pyramiid(double alus, double korgus, double kKorgus){
+		super(korgus);
+		a=alus;
+		kK=kKorgus;
+	}
+	@Override
+	public double pohjaPindala(){
+		return a*a;
+	}
+	public double kuljePindala(){
+		return ((a*kK)/2)*4;
+	}
+}
---
+++ src/test/java/ats/pyramiidiTest.java
@@ -0,0 +1,17 @@
+package ats;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class pyramiidiTest{
+		@Test
+		public void ruumala(){
+			pyramiid r2=new pyramiid(5.0,5.0,5.0);
+			assertEquals(62.5,r2.ruumala(), 0.01);
+		}
+		@Test
+		public void kuljePindala(){
+			pyramiid r3=new pyramiid(5.0,5.0,5.0);
+			assertEquals(50.0,r3.kuljePindala(), 0.01);
+		}
+}
